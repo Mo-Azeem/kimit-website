81c14b3a4e0c48d522530d58a6fe3cd3341320c5
m.abdelazeem <m.abdelazeem@dscoola.com>
2023-04-06 05:10:53 +0200

    feat: software services independed component

--- Components/SoftwareServiceHero/index.jsx
+++ Components/SoftwareServiceHero/index.jsx
@@ -1,15 +1,64 @@
-import React from "react";
-import Home from "../Home/Home";
-import style from "./SoftwareServiceHero.module.css";
+import React, { useEffect } from "react";
+import style from "./Home.module.css";
+import { useRouter } from "next/router";
 
-export default function SoftwareServiceHero() {
+export default function Home(props) {
+  const router = useRouter();
+
+  function onScroll() {
+    let element = document.querySelector(".navbar");
+    if (element) {
+      element.style.backgroundImage = "url('../../background2.jpg')";
+    }
+  }
+  useEffect(() => {
+    let element = document.querySelector(".navbar");
+    element.style.backgroundImage = "url('../../background2.jpg')";
+    window.addEventListener("scroll", onScroll, { passive: true });
+  }, []);
+
+  function navigate(to) {
+    router.push(`./${to}`);
+  }
   return (
-    <div
-      style={{ backgroundImage: `url("./homeSoftware.png")` }}
-      className={` ${style.background} w-100 `}
-    >
-      <div className={`h-100 w-100 d-flex align-items-center`}>
-        <Home page="Tearm" />
+    <div className={`${style.contpos}  h-100 w-100 d-flex align-items-center`} style={{
+        background: `url('../../homeSoftware.png') no-repeat`,
+        backgroundSize: 'cover',
+    }}>
+      <div className={` ${style.home} container pb-5 mt-5 `}>
+        <div className="row gy-3 mt-2  w-100  pb-5">
+          <div className="col-lg-6 d-flex align-items-center ">
+            <div className="des w-100">
+                <div className="">
+                  <span
+                    className={`${style.titleSoftware} ${style.firstTitle}`}
+                  >
+                    The <span className={`${style.spanSoft}`}>Best</span> Place
+                    To{" "}
+                  </span>
+                  <br />
+                  <span
+                    className={`${style.titleSoftware} ${style.secondTitle} `}
+                  >
+                    {" "}
+                    Start Your Business
+                  </span>
+                </div>
+                <h4 className={`${style.titleDescSoftware} ${style.titleDesc}`}>
+                  {" "}
+                  There are many variations of passages of Lorem Ipsum <br />
+                  but the majority have suffered alteration
+                </h4>
+              <button
+                onClick={() => navigate("Academy")}
+                className={`${style.btn} signupBtn btn btn-danger  ms-0 `}
+              >
+                Get Started
+              </button>
+            </div>
+          </div>
+          
+        </div>
       </div>
     </div>
   );
